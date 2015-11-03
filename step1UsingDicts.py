8a59ef303bbb6b84c3b9088a32690e937784b944
liggettla <alexander.liggett@gmail.com>
2015-11-03 13:11:24 -0700

    Progress

---
+++ step1UsingDicts.py
@@ -0,0 +1,7 @@
+#Uses python 3.x
+
+#using dictionaries is typically more convenient than using field labels
+bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
+sue = {'name': 'Sue Jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
+print(bob['name'], sue['pay'])
+print(bob['name'].split()[-1]) #print last name
