c66437c68d74e76c15e34e527d460819a5d78c4f
liggettla <alexander.liggett@gmail.com>
2015-11-03 13:20:48 -0700

    Fixed cluster localization

--- step1UsingDicts.py
+++ step1UsingDicts.py
@@ -1,7 +1,23 @@
 #Uses python 3.x
 
 #using dictionaries is typically more convenient than using field labels
+print('\nUsing a simple dictionary')
 bob = {'name': 'Bob Smith', 'age': 42, 'pay': 30000, 'job': 'dev'}
 sue = {'name': 'Sue Jones', 'age': 45, 'pay': 40000, 'job': 'hdw'}
 print(bob['name'], sue['pay'])
 print(bob['name'].split()[-1]) #print last name
+
+#because dicts are so useful there are even faster ways of creating them
+#the following can be used so long as all the keys are strings
+print('\nOther ways of creating dictionaries')
+bob = dict(name = 'Bob Smith', age = 42, pay = 30000, job = 'dev')
+sue = dict(name = 'Sue Jones', age = 45, pay = 40000, job = 'hdw')
+print(bob)
+
+print('\nDictionaries can be also created one field at a time')
+sue = {}
+sue['name'] = 'Sue Jones'
+sue['age'] = 45
+sue['pay'] = 40000
+sue['job'] = 'hdw'
+print(sue)
