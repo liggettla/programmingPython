bf60c32671c79bed15986372ae4357ea42ab950c
liggettla <alexander.liggett@gmail.com>
2015-11-06 14:53:28 -0700

    Progress

--- step1UsingDicts.py
+++ step1UsingDicts.py
@@ -1,4 +1,5 @@
 #Uses python 3.x
+import pprint
 
 #using dictionaries is typically more convenient than using field labels
 print('\nUsing a simple dictionary')
@@ -58,3 +59,71 @@ print(names)
 print(list(map((lambda x: x['name']), people))) #collect names
 
 print(sum(person['pay'] for person in people)) #sum all pay
+
+#list comprehensions can approach the utility of SQL queries
+print([rec['name'] for rec in people if rec['age'] >= 45])
+print([(rec['age'] ** 2 if rec['age'] >= 45 else rec['age']) for rec in people])
+
+G = (rec['name'] for rec in people if rec['age'] >= 45)
+print(next(G))
+
+G = ((rec['age'] ** 2 if rec['age'] >= 45 else rec['age']) for rec in people)
+print(next(G)) #does the same as the following line
+print(G.__next__())
+
+#because dictionaries are objects, they can be accesed and updated
+print('\nDictionaries can be updated')
+for person in people:
+    print(person['name'].split()[-1]) #print last name
+    person['pay'] *= 1.10
+    print(person['pay'])
+
+#nested structures
+print('\nOne of the advantages of scripting languages is that they can build nested structures and later reclaim the memory after this has been done')
+bob2 = {'name':{'first':'Bob','last':'Smith'}, 'age': 42, 'job': ['software', 'writing'], 'pay': (40000, 50000)}
+#now simply index twice to get a value that is two levels deep
+print(bob2['name']) #full name
+print(bob2['name']['last']) #last name
+bob2['pay'][1] #upper pay
+
+for job in bob2['job']: print(job)
+bob2['job'][-1] #bob's last job
+bob2['job'].append('janitor') #time for a new job
+for job in bob2['job']: print(job)
+
+#dicts of dicts
+print('\nDictionaries can also be made out of dictionaries')
+bob = dict(name='Bob Smith', age=42, pay=30000, job='dev')
+sue = dict(name='Sue Jones', age=45, pay=40000, job='hdw')
+print(sue)
+db = {}
+db['bob'] = bob
+db['sue'] = sue
+print(db)
+print(db['bob']['name'])
+print(db['sue']['pay'])
+db['sue']['pay'] = 50000 #change value
+print(db['sue']['pay'])
+pprint.pprint(db) #pretty print clearly displays nested data structures
+
+print('\nIterating through nested dictionaries')
+for key in db:
+    print(key, 'salary =>', db[key]['pay'])
+
+#visiting all records by key
+for key in db:
+   print(db[key]['name'].split()[-1])
+   db[key]['pay'] *= 1.10
+
+for record in db.values(): print(record['pay'])
+
+#adding records with new keys
+print('\nAdding records with new keys')
+db['sue']['pay'] = 50000 #change value
+pprint.pprint(db)
+db['tom'] = dict(name='Tom', age=50, job=None, pay=0)
+print('\n')
+pprint.pprint(db)
+print('length of db:', len(db))
+
+#pg 56
