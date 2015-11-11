18d623d6d0cbdec348c0560c739f720f474080b9
liggettla <alexander.liggett@gmail.com>
2015-11-11 10:55:18 -0700

    Progress

--- step2FormattedFiles.py
+++ step2FormattedFiles.py
@@ -15,5 +15,53 @@ if __name__ == '__main__':
     for key in db:
         print(key, '=>\n ', db[key])
 
-dbfilename = 'people-file'
+'''
+The following portion of the script saves in-memory database info to a file
+with some custom formatting. 'endrec.', 'enddb.', and '=>' must not be used
+in the data. db is a dictionary of dictionaries
+This is also bad code because it rus strings as code.
+Could also use: dbfile.write(key + '\n') instead of print(key, file=dbfile);
+'''
 
+dbfilename = 'people-file'
+ENDDB = 'enddb.'
+ENDREC = 'endrec.'
+RECSEP = '=>'
+
+def storeDbase(db, dbfilename=dbfilename):
+    "formatted dump of database to flat file"
+    dbfile = open(dbfilename, 'w')
+
+    for key in db:
+        print(key, file=dbfile)
+        for (name, value) in db[key].items():
+            print(name + RECSEP + repr(value), file=dbfile)
+        print(ENDREC, file=dbfile)
+    print(ENDDB, file=dbfile)
+    dbfile.close()
+
+def loadDbase(dbfilename=dbfilename):
+    "parse data to reconstruct database from output file"
+    dbfile = open(dbfilename)
+    import sys
+    sys.stdin = dbfile
+    db = {}
+    key = input()
+    while key != ENDDB:
+        rec = {}
+        field = input()
+        while field != ENDREC:
+            name, value = field.split(RECSEP)
+            rec[name] = eval(value)
+            field = input()
+        db[key] = rec
+        key = input()
+    return db
+
+storeDbase(db, dbfilename)
+
+'''
+if __name__ == '__main__':
+    from initdata import db
+    storeDbase(db)
+    '''
