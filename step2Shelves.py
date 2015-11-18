f996306dc773cadef9f1ada186a17c1255aa90b0
liggettla <alexander.liggett@gmail.com>
2015-11-18 15:56:27 -0700

    Progress

--- step2Shelves.py
+++ step2Shelves.py
@@ -9,9 +9,32 @@ import shelve
 
 bob = {'name':'Bob Smith', 'age':42, 'pay':30000, 'job':'dev'}
 sue = {'name':'Sue Jones', 'age':45, 'pay':40000, 'job':'hdw'}
+dbTemp = {}
+dbTemp['bob'] = bob
+dbTemp['sue'] = sue
+print('This is the structure of the shelve')
+print(dbTemp)
 
 shelveOut = './output/people-shelve'
 db = shelve.open(shelveOut)
 db['bob'] = bob
 db['sue'] = sue
+db.close()
+
+db = shelve.open(shelveOut)
+print('\nThis is accessing a record from the shelve')
+print(db['bob'])
+db.close()
+
+#shelves are edited just like dictionaries
+print('\nThis is editing a single record')
+db = shelve.open(shelveOut)
+sue = db['sue']
+sue['pay'] *= 1.50
+db['sue'] = sue
+print(db['sue'])
+#The following does not work with shelves
+#data must be read into memory, modified there and then written back to shelve
+#db['sue']['pay'] = 90000
+print(db['sue']['pay'])
 db.close()
