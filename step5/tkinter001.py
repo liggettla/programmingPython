cf3e25a10a7e4a50bb1a8d262c077810dc44a394
liggettla <alexander.liggett@gmail.com>
2015-11-24 13:18:21 -0700

    Progress

---
+++ step5/tkinter001.py
@@ -0,0 +1,14 @@
+#This is just a basic intro script showing the use of tkinter
+
+#for python 2.x
+#from Tkinter import *
+
+from tkinter import *
+'''
+pack is a geometry manager method
+Label constructs the entire widget
+mainloop starts the event processing
+'''
+
+Label(text='Spam').pack()
+mainloop()
